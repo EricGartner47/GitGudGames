c0c9947c74a5201a6e45f61d5d2f6efea802367e
EricGartner47 <egartner47@gmail.com>
2022-01-20 17:44:18 -0500

    working on shelf form

--- react-app/src/components/ShelfFormNew/index.js
+++ react-app/src/components/ShelfFormNew/index.js
@@ -1,11 +1,27 @@
 import React, {useState} from 'react';
 import { useSelector, useDispatch} from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import './ShelfFormNew.css'
 
 const ShelfFormNew = () => {
     const dispatch = useDispatch();
+    const user = useSelector(state => state.session.user)
     const [title, setTitle] = useState('')
     const [errorrs, setErrors] = useState([])
+
+    if(!user) {
+        return (
+            <Redirect to='/' />
+        )
+    }
+
+    return (
+        <>
+            
+
+
+        </>
+    )
 }
 
 export default ShelfFormNew
